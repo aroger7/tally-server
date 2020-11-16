57cbd4b8c6d2a5f92d2641a69b0e6672b05d9735
Adam Rogers <cldd5678@hotmail.com>
2020-11-16 12:38:49 -0500

    initial graphql commit

--- amplify/backend/function/TallyApiFunction/src/app.js
+++ amplify/backend/function/TallyApiFunction/src/app.js
@@ -4,31 +4,65 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const apps = require('./routes/apps');
+// const apps = require('./routes/apps');
 // const playerCounts = require('./routes/playerCounts');
-const { getDatabase } = require('./middleware/db');
+// const { getDatabase } = require('./middleware/db');
 
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: (parent, args, context) => {
+      console.log(context.req.body);
+      return 'Hello world!'
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({ 
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+  context: ({ req }) => ({
+    req
+  })
+});
+const middlewares = apolloServer.getMiddleware();
+console.log(middlewares);
 const app = express();
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    allowedHeaders: ['x-auth', 'Content-Type'],
-    exposedHeaders: ['x-auth']
-  })
-);
-app.use(express.static(path.resolve(__dirname, '../../dist')));
-app.use(bodyParser.json());
-app.use(getDatabase);
-app.get('/', async (req, res) => {
-  const app = await req.db.models.App.findByPk(39210);
-  console.log(app.name);
-  // console.log(req.db);
-  res.sendStatus(200);
-})
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     allowedHeaders: ['x-auth', 'Content-Type'],
+//     exposedHeaders: ['x-auth']
+//   })
+// );
+// app.use(express.static(path.resolve(__dirname, '../../dist')));
+// app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//   console.log(req.body);
+//   next()
+// });
+// app.use(getDatabase);
+apolloServer.applyMiddleware({ app });
+// app.use((req, res, next) => {
+//   Object.keys(req).forEach(key => console.log(key));
+//   console.log(req.body);
+//   // console.log(req.body);
+//   next();
+// });
 
-app.use('/app', apps);
+// app.use('/app', apps);
 // app.use('/playercount', playerCounts);
 
 module.exports = { app };
--- amplify/backend/function/TallyApiFunction/src/index.js
+++ amplify/backend/function/TallyApiFunction/src/index.js
@@ -1,43 +1,84 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const http = require('http');
-const middy = require('middy');
+const middy = require('@middy/core');
 const AWS = require('aws-sdk');
 const secretsManager = require('@middy/secrets-manager');
-const { app } = require('./app');
+const cors = require('@middy/http-cors');
+// const { app } = require('./app');
 const config = require('./config');
 
 AWS.config.update({ region: 'us-east-1' });
 
-let server = null;
+// let server = null;
 
-if (process.env.ENV) {
-  server = awsServerlessExpress.createServer(app);
-} else {
-  server = http.createServer(app);
-  server.listen(3000, () => console.log('Listening on port 3000!'));
-}
+// if (process.env.ENV) {
+//   server = awsServerlessExpress.createServer(app);
+// } else {
+//   server = http.createServer(app);
+//   server.listen(3000, () => console.log('Listening on port 3000!'));
+// }
 
-const handler = middy(async (event, context) => {
-  config.DB_HOST = context.DB_CREDENTIALS.host;
-  config.DB_USERNAME = context.DB_CREDENTIALS.username;
-  config.DB_PASSWORD = context.DB_CREDENTIALS.password;
-  config.DB_NAME = 'postgres';
-  
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
-}).use(secretsManager({
-  cache: true,
-  region: 'us-east-1',
-  secrets: {
-    DB_CREDENTIALS: config.DB_SECRET_NAME
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
   }
-}));
+`;
 
-const localInvoke = async () => {
-  await handler({}, {}, () => null);
-}
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
 
-if (!process.env.ENV) {
-  localInvoke();
-}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  
+  // By default, the GraphQL Playground interface and GraphQL introspection
+  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+  //
+  // If you'd like to have GraphQL Playground and introspection enabled in production,
+  // the `playground` and `introspection` options must be set explicitly to `true`.
+  playground: true,
+  introspection: true,
+});
 
-exports.handler = handler;
+exports.handler = middy(server.createHandler())
+  .use(cors())
+  .use(secretsManager({
+    cache: true,
+    region: 'us-east-1',
+    secrets: {
+      DB_CREDENTIALS: config.DB_SECRET_NAME
+    }
+  }))
+  .use(testMiddleware());
+
+// const handler = middy(async (event, context) => {
+//   config.DB_HOST = context.DB_CREDENTIALS.host;
+//   config.DB_USERNAME = context.DB_CREDENTIALS.username;
+//   config.DB_PASSWORD = context.DB_CREDENTIALS.password;
+//   config.DB_NAME = 'postgres';
+  
+//   return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+// }).use(secretsManager({
+//   cache: true,
+//   region: 'us-east-1',
+//   secrets: {
+//     DB_CREDENTIALS: config.DB_SECRET_NAME
+//   }
+// }));
+
+// const localInvoke = async () => {
+//   await handler({}, {}, () => null);
+// }
+
+// if (!process.env.ENV) {
+//   localInvoke();
+// }
+
+// exports.handler = handler;
