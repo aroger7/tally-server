ea5e9e0266a1cf3ef38b2989768fc33ea378b046
Adam Rogers <adam@adamrogers.ca>
2021-04-02 22:37:50 -0400

    add header to app

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -2,8 +2,13 @@ import styled from '@emotion/styled';
 import { colors } from 'src/styles';
 
 export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   background-color: ${colors.almostBlack};
   width: 100%;
   height: 100%;
   box-shadow: 0 0 10px;
+  padding: 1rem;
+  box-sizing: border-box;
 `;
