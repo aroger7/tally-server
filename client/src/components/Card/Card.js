d8ccc9aa9940fc506eae3a912a8900e88c7b078a
Adam Rogers <adam@adamrogers.ca>
2021-04-29 22:13:35 -0400

    add Card component

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import { colors } from 'src/styles';
+
+const Card = styled.div`
+  background-color: ${colors.sapphire};
+  border-radius: 5px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export default Card;
