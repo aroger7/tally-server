e5b641b3c3785792cd68f6f12ac438e69e22082a
Adam Rogers <adam@adamrogers.ca>
2021-04-29 22:14:52 -0400

    add colors

--- client/src/styles/colors.js
+++ client/src/styles/colors.js
@@ -4,7 +4,11 @@ const colors = {
   almostWhite: '#FAFAFA',
   almostBlack: '#1A1A1D',
   jacksonPurple: '#1F3C97',
-  cornflowerBlue: '#6290F9'
+  cornflowerBlue: '#6290F9',
+  sapphire: '#3550A1',
+  lightGrey: '#C8C8C8',
+  green: '#00CC00',
+  red: '#CC0000'
 }
 
 export default {
