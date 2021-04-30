a93ef99f1cd6f56f23d3fc6eadf05311efe893ee
Adam Rogers <adam@adamrogers.ca>
2021-04-29 22:14:42 -0400

    use grid in App layout

--- client/src/components/App/styles.js
+++ client/src/components/App/styles.js
@@ -2,17 +2,21 @@ import styled from '@emotion/styled';
 import { colors } from 'src/styles';
 
 export const App = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 4rem 1fr;
+  width: 100vw;
+  height: 100vh;
   background-color: ${colors.primary};
 `;
 
 export const HeaderContainer = styled.div`
-  flex: 0 0 4rem;
+  grid-column: 1;
+  grid-row: 1;
 `;
 
 export const Body = styled.div`
-  flex: 1;
+  grid-column: 1;
+  grid-row: 2;
   overflow-y: auto;
 `;
