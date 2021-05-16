e3341d7c2760abe0e238e7feafb3bdcb8220320a
Adam Rogers <adam@adamrogers.ca>
2021-05-15 23:20:44 -0400

    add appMonth

--- services/stats-api/typeDefs/index.js
+++ services/stats-api/typeDefs/index.js
@@ -3,9 +3,10 @@ const root = require('./root');
 const app = require('./app');
 const appList = require('./appList');
 const appDay = require('./appDay');
+const appMonth = require('./appMonth');
 const pagedList = require('./pagedList');
 const playerCount = require('./playerCount');
 
-const typeDefs = [root, app, appList, appDay, pagedList, playerCount];
+const typeDefs = [root, app, appList, appDay, appMonth, pagedList, playerCount];
 
 module.exports = typeDefs.map(x => print(x));
